539de181cc51da24a207eee35c3b2ea803769421
Sprinter31 <alfret008@web.de>
2025-06-27 23:07:00 +0200

    update arduino skript

--- InoSkript/InoSkript.ino
+++ InoSkript/InoSkript.ino
@@ -1,14 +1,50 @@
-int relaisPin = 13;
+bool shouldBlink = false;
+bool isLedOn = false;
 
 void setup() 
 {
-    pinMode(relaisPin, OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() 
 {
-    digitalWrite(relaisPin, HIGH);
-    delay(200);
-    digitalWrite(relaisPin, LOW);
-    delay(100); 
+  int delayTime = 500;
+
+  if (Serial.available() > 0) 
+  {
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+
+    if (input.indexOf("LED_ON") != -1) // input.Contains()
+    {
+      shouldBlink = true;
+
+      int sepIndex = input.indexOf(':');
+      if (sepIndex != -1) 
+      {
+        String numberPart = input.substring(sepIndex + 1);
+        delayTime = numberPart.toInt();
+      }
+    } 
+    else if (input == "LED_OFF") 
+    {
+      shouldBlink = false;
+    }
+  }
+
+  if(shouldBlink)
+  {
+    if(isLedOn)
+    {
+      digitalWrite(13, LOW);      
+      isLedOn = false;
+    }
+    else
+    {
+      digitalWrite(13, HIGH);
+      isLedOn = true;
+    }
+  }
+  delay(delayTime);
 }
