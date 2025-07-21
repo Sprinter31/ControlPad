b18b5e2bea6cfd1d38622310123e7ac6aa200aac
PalmarHealer <myoserhd111@gmail.com>
2025-07-22 00:22:27 +0200

    Update Arduino Controller

--- ControlPad/Arduino/ArduinoController.cs
+++ ControlPad/Arduino/ArduinoController.cs
@@ -6,7 +6,7 @@ using System.Windows;
 using System.Windows.Controls;
 
 namespace ControlPad
-{   
+{
     public class ArduinoController
     {
         private HomeUserControl MUC;
@@ -23,27 +23,27 @@ namespace ControlPad
             {
                 { MUC.Slider1, 0 },
                 { MUC.Slider2, 0 },
-                /*{ mw.Slider3, 0 },
-                { mw.Slider4, 0 },
-                { mw.Slider5, 0 },
-                { mw.Slider6, 0 },
-                { mw.Switch1, 0 },
-                { mw.Switch2, 0 },
-                { mw.Switch3, 0 },
-                { mw.Switch4, 0 },
-                { mw.Switch5, 0 },
-                { mw.Switch6, 0 },
-                { mw.Switch7, 0 },
-                { mw.Switch8, 0 },
-                { mw.Switch9, 0 },
-                { mw.Switch10, 0 },
-                { mw.Switch11, 0 }*/
+                { MUC.Slider3, 0 },
+                { MUC.Slider4, 0 },
+                { MUC.Slider5, 0 },
+                { MUC.Slider6, 0 },
+                { MUC.Switch1, 0 },
+                { MUC.Switch2, 0 },
+                { MUC.Switch3, 0 },
+                { MUC.Switch4, 0 },
+                { MUC.Switch5, 0 },
+                { MUC.Switch6, 0 },
+                { MUC.Switch7, 0 },
+                { MUC.Switch8, 0 },
+                { MUC.Switch9, 0 },
+                { MUC.Switch10, 0 },
+                { MUC.Switch11, 0 }
             };
 
             string port = ArduinoPortFinder.FindFirstArduinoPort();
             if (port != null)
             {
-                _serialPort = new SerialPort(port, 9600);
+                _serialPort = new SerialPort(port, 115200);
                 _serialPort.DataReceived += SerialPort_DataReceived;
                 _serialPort.Open();
             }
@@ -53,44 +53,47 @@ namespace ControlPad
 
         private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            try
-            {
-                string line = _serialPort.ReadLine().Replace("\r", "");
-                if (!Regex.IsMatch(line, @"^\s*-?\d+\s*(,\s*-?\d+\s*){3}$")) return;
+                try
+                {
+                    string line = _serialPort.ReadLine().Replace("\r", "");
+                    string[] inputs = Regex.Split(line, ",");
 
-                string[] inputs = line.Split(',');
+                    if (inputs.Length < 16) return;
 
-                UpdateValues(inputs);
-                MUC.Dispatcher.Invoke(() => eventHandler.Update(values));
-            }
-            catch (IOException ex)
-            {
-                OnSerialPortDisconnected(ex.ToString());
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"EX: {ex}");
-            }
-        }  
+
+                    UpdateValues(inputs);
+
+                    MUC.Dispatcher.BeginInvoke(() => eventHandler.Update(values));
+
+                }
+                catch (IOException)
+                {
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"EX: {ex}");
+                }
+        }
 
         private void UpdateValues(string[] inputs)
         {
             values[MUC.Slider1] = int.Parse(inputs[0]);
             values[MUC.Slider2] = int.Parse(inputs[1]);
-            /*values[mw.Slider3] = int.Parse(inputs[2]);
-            values[mw.Slider4] = int.Parse(inputs[3]);
-            values[mw.Slider5] = int.Parse(inputs[4]);
-            values[mw.Slider6] = int.Parse(inputs[5]);
-            values[mw.Switch1] = int.Parse(inputs[6]);
-            values[mw.Switch3] = int.Parse(inputs[7]);
-            values[mw.Switch4] = int.Parse(inputs[8]);
-            values[mw.Switch5] = int.Parse(inputs[9]);
-            values[mw.Switch6] = int.Parse(inputs[10]);
-            values[mw.Switch7] = int.Parse(inputs[11]);
-            values[mw.Switch8] = int.Parse(inputs[12]);
-            values[mw.Switch9] = int.Parse(inputs[13]);
-            values[mw.Switch10] = int.Parse(inputs[14]);
-            values[mw.Switch11] = int.Parse(inputs[15]);*/
+            values[MUC.Slider3] = int.Parse(inputs[2]);
+            values[MUC.Slider4] = int.Parse(inputs[3]);
+            values[MUC.Slider5] = int.Parse(inputs[4]);
+            values[MUC.Slider6] = int.Parse(inputs[5]);
+            values[MUC.Switch1] = int.Parse(inputs[6]);
+            values[MUC.Switch3] = int.Parse(inputs[7]);
+            values[MUC.Switch4] = int.Parse(inputs[8]);
+            values[MUC.Switch5] = int.Parse(inputs[9]);
+            values[MUC.Switch6] = int.Parse(inputs[10]);
+            values[MUC.Switch7] = int.Parse(inputs[11]);
+            values[MUC.Switch8] = int.Parse(inputs[12]);
+            values[MUC.Switch9] = int.Parse(inputs[13]);
+            values[MUC.Switch10] = int.Parse(inputs[14]);
+            values[MUC.Switch11] = int.Parse(inputs[15]);
         }
 
         private void OnSerialPortDisconnected(string ex)
